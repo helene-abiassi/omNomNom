797e1153bc2cb7d24d022621e40ffdf21d9177c6
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-09-17 21:03:18 +0200

    Update and rename SignupLogin.tsx to Signup.tsx

---
+++ src/views/Signup.tsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+import '../src/style/SignupPage.css'
+import SignUp from "../components/SignUp"
+// import LogIn from "../components/LogIn"
+
+
+
+function Signup() {
+  return (
+    <div>
+    <SignUp />
+
+
+    </div>
+  )
+}
+
+export default Signup
+
+
--- src/views/SignupLogin.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function SignupLogin() {
-  return (
-    <div>SignupLogin</div>
-  )
-}
-
-export default SignupLogin
