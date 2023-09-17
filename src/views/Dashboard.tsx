3c1bf8b840632ef589b7059f97a47a2d9eac7605
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-09-17 21:01:38 +0200

    Update Dashboard.tsx

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+// import React from 'react'
+import '../src/style/dashboard.css'
+import { useNavigate } from 'react-router-dom'
 
 function Dashboard() {
+
+  const dashNavigateTo = useNavigate()
+
+const goBrowseDash = ()=>{
+  dashNavigateTo("/browse")
+}
+
+const goMyRecipesDash = ()=>{
+  dashNavigateTo("/my-recipes")
+}
+
+const goLogOutDash = ()=>{
+  dashNavigateTo("/")
+}
+
   return (
-    <div>Dashboard</div>
+    <div className="dashboard" style={{height: "60vh" }}>
+      <h2>Welcome {"user.name"}</h2>
+      <button onClick={goBrowseDash} className="dashboardBtn">Browse</button>
+      <button onClick={goMyRecipesDash} className="dashboardBtn"> My Recipes</button>
+      <button onClick={goLogOutDash} className="dashboardBtn">Log out</button>
+
+<a style={{marginTop: "5rem" }} href="/">Delete your account</a>
+    
+
+
+
+    </div>
   )
 }
 
