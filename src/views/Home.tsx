7020e5150acb9a22bdd20c6f1b32869d7acca9da
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-09-17 21:02:15 +0200

    Update Home.tsx

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+// import React from 'react'
+// import BrowseRecipes from './BrowseRecipes'
 
 function Home() {
   return (
-    <div>Home</div>
+    <> <div style={{height: "80vh" }} >
+      <img className="homePageImg" src="./public/pasta.webp" alt="" />
+<p style={{ alignContent: 'center'}} >Look for fun and colorful recipes, based off 
+  what is <br />left in your fridge, your favorite cuisine 
+  or preferred diet!</p>
+  <p>Become a member and build 
+your own hangry pantry!</p>
+
+    <button><a href="browse">Let's go!</a></button>
+ </div>
+    </>
   )
 }
 
