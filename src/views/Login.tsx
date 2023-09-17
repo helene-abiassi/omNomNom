662a4dfd922fc58b004ccd91967fe952f4d41950
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-09-17 21:03:47 +0200

    Add files via upload

---
+++ src/views/Login.tsx
@@ -0,0 +1,19 @@
+import '../src/style/SignupPage.css'
+// import SignUp from "../components/SignUp"
+import LogIn from "../components/LogIn"
+
+
+
+function Login() {
+  return (
+    <>
+    <LogIn />
+
+
+    </>
+  )
+}
+
+export default Login
+
+
