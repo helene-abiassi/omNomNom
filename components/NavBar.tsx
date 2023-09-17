7b4ce7217482d61f8ccf6c655fc10926bee0e19b
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-09-17 20:56:40 +0200

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,41 +1,31 @@
 // import React from 'react'
+import { NavLink } from "react-router-dom"
 
-function Login() {
+function NavBar() {
   return (
-    <div>
-        <br />
-        <div className="container">
-    <form className="signUpForm" action="/action_page.php">
-      {/* <p>Please fill in the information below to create an account:</p> */}
-      <p>Fill in the information below to log in:</p>
+    <>
+        <div className="navBar">
+            <nav>
+                <div className="navLeft">
+                  <NavLink to="/"><i className="fa fa-home"></i></NavLink>
+                  <span> | </span>
+                  <NavLink to="browse">Browse</NavLink>
+                  <span> | </span>
+                  <NavLink to="my-recipes">My Recipes</NavLink>
+                </div>
+                {/* <span> | </span> */}
+                <div className="loginNavBar">
+                  <NavLink to="login">Log in </NavLink><span>|</span> <NavLink to="signup"> Sign up</NavLink>
+                  </div>
+            </nav>
 
-      <label htmlFor="email">Email:</label>
-      <input className="searchInputBox" type="text" placeholder='Enter e-mail...' name='email' required />
-      <br /><label htmlFor="psw">Password:</label>
-
-        <input className="searchInputBox" type="password" placeholder='Enter password...' name='psw' required />
-
-<br />
-<a href="#">Log in with your Google account</a>
-      
-    
-<br />
-      <label>
-      <input type="checkbox" checked={true} name="remember"/> Remember me
-    </label><br />
-      <button type="submit" className="signupbtn">Login</button>
-    </form>
-{/* //!Use NavigateTo after submit button to dashboard */}
-
-  <img className="loginImg" src="./public/DONUT-copy.png" alt="" />
-</div><br />
-<p >Don't have an account yet? <a href="signup">Sign up!</a></p>
-
-
-<br />
-
-</div>
+            <header>
+                <img className="mainLogo" src="./public/logo.png" alt="" />
+            </header>
+        </div>
+        
+    </>
   )
 }
 
-export default Login
+export default NavBar
