1451feabc90126d4b0f2533ea2ff11bf4911d752
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-09-20 00:49:01 +0200

    Update RecipeCards.tsx

--- components/RecipeCards.tsx
+++ components/RecipeCards.tsx
@@ -1,24 +1,23 @@
 // import { Link } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { RecipeType } from './RecipesGrid';
+import { RecipeCardProp } from '../src/types/customTypes';
 
 
-export interface RecipeCardProp {
-  recipe: RecipeType;
-}
 
 function RecipeCards({recipe}: RecipeCardProp) {
 
 
-  const {id, image, title} = recipe;
+  const {id, image, title, readyInMinutes, servings} = recipe;
+  // console.log('recipe.title :>> ', recipe.title);
 
   return (
     <>
       <div className="RecipeCard">
         <button className='favIcon'>❤ 0</button>
                     <img className= "RecipeImage" src={image} alt={title} key={id} />
-                    <h3>{title}</h3><br />
-                    <Link style={{backgroundColor:"black",  justifyItems:"baseline", alignSelf:"center", borderRadius:"10px", width:"8rem"}} to={`${recipe.id}`}><h4>View More</h4></Link>
+                    <h3>{title}</h3>
+                    <p style={{textAlign:"left", marginLeft:"2rem"}}>Ready in {readyInMinutes} mns. Serves {servings} humans</p>
+                    <Link className='linkButton' to={`${recipe.id}`} state={{ recipe:recipe }}><h4>View More</h4></Link>
                 </div>
 
     </>
@@ -26,3 +25,5 @@ function RecipeCards({recipe}: RecipeCardProp) {
 }
 
 export default RecipeCards
+
+
