d0480b77c30916f5b53ee2e3cb44270d2b4bb9f0
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-09-20 00:49:24 +0200

    Update RecipesGrid.tsx

--- components/RecipesGrid.tsx
+++ components/RecipesGrid.tsx
@@ -1,59 +1,94 @@
 // import React from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import RecipeCards from './RecipeCards'
 import SearchBox from './SearchBox'
+import { RecipeType } from '../src/types/customTypes';
+import { RecipeContext, RecipeContextProvider } from '../src/context/RecipeContext';
 
 
 
-export interface RecipeType {
-    id: number,
-    title: string,
-    image: string,
-    imageType: string,
-}
 
 
 function RecipesGrid() {
 
-const [recipes, setRecipes] = useState<RecipeType[]>([
-    {
-        id: 0,
-        title: "",
-        image: "",
-        imageType: "",
-    }
-])
+  // const {recipes, query, cuisine, diet,  fetchRecipes, setQuery, 
+  // setCuisine, setDiet, changePageNumber} = useContext(RecipeContext)
 
-const [offset, setOffset] = useState<number>(0)
-const [query, setQuery] = useState<string>("")
+  // console.log('contextValues :>> ', contextValues);
+
+
+  const [recipes, setRecipes] = useState<RecipeType[]>([
+    {
+      id: 0,
+      title: "",
+      image: "",
+      imageType: "",
+      healthScore: 0,
+      readyInMinutes:0,
+      servings:0,
+      analyzedInstructions:[
+        {
+          steps: [
+            {
+              number:0,
+              steps:"",
+              length:{
+                number:0,
+                unit:"",
+              }
+            }
+          ]
+      }],
+      extendedIngredients: [
+        {
+          original:"",
+          measures:{
+            metric:{
+              amount:0, 
+              unitShort:"",
+            }
+          }
+        }
+      ]
+      
+  }
+  ])
+
+
+  const [offset, setOffset] = useState<number>(0)
+  const [query, setQuery] = useState<string>("")
 const [cuisine, setCuisine] = useState<string>("")
 const [diet, setDiet] = useState<string>("")
+  
 
+  const fetchRecipes = async () => {
+    const url= `https://api.spoonacular.com/recipes/complexSearch?offset=${offset}&number=24&query=${query}&diet=${diet}&cuisine=${cuisine}&addRecipeInformation=true&fillIngredients=true&apiKey=${apiKey}`
+    
+        const response = await fetch(url);
+        const data = await response.json();
+        // console.log(data);
+        const recipesList = data.results as RecipeType[];
+        console.log(recipesList);
+        setQuery(query)
+        setCuisine(cuisine)
+        setDiet(diet)
+        setRecipes(recipesList);
+      };
 
-const fetchRecipes = async () => {
-const url=  `https://api.spoonacular.com/recipes/complexSearch?offset=${offset}&number=24&query=${query}&diet=${diet}&cuisine=${cuisine}&apiKey=${apiKey}`
-    const response = await fetch(url);
-    const data = await response.json();
-    // console.log(data);
-    const recipesList = data.results as RecipeType[];
-    // console.log(recipesList);
-    setQuery(query)
-    setCuisine(cuisine)
-    setDiet(diet)
-    setRecipes(recipesList);
-  };
+      const changePageNumber = (e: React.MouseEvent<HTMLButtonElement>) => {
+        const eventTarget = e.target as HTMLButtonElement;
+        const buttonClicked = eventTarget.value;
+        if (buttonClicked === "next") {
+          setOffset(offset + 24);
+        }
+        if (buttonClicked === "prev") {
+          setOffset(offset - 24);
+        }
+      };
+    
 
-  const changePageNumber = (e: React.MouseEvent<HTMLButtonElement>) => {
-    // console.log("e :>> ", e);
-    const eventTarget = e.target as HTMLButtonElement;
-    const buttonClicked = eventTarget.value;
-    if (buttonClicked === "next") {
-      setOffset(offset + 24);
-    }
-    if (buttonClicked === "prev") {
-      setOffset(offset - 24);
-    }
-  };
+  
+
 
   useEffect(() => {
     fetchRecipes();
@@ -69,8 +104,8 @@ const url=  `https://api.spoonacular.com/recipes/complexSearch?offset=${offset}&
   {recipes ? (
     recipes.map((recipe) => {
       return (
-        <div key={recipe.id} >
-          <RecipeCards recipe ={recipe} query={query} cuisine={cuisine} diet={diet} offset={offset} />
+        <div  >
+          <RecipeCards key={recipe.id} recipe ={recipe} query={query} cuisine={cuisine} diet={diet} offset={offset}/>
         </div>
       );
     })
