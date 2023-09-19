b749d1d1b1f8c0282b55fddfb5dd59461f40625f
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-09-20 00:51:17 +0200

    Update Home.tsx

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,17 +1,25 @@
 // import React from 'react'
 // import BrowseRecipes from './BrowseRecipes'
 
+import { useContext } from "react"
+import { AuthContext } from "../src/context/AuthContext"
+
 function Home() {
+
+  const {user}= useContext(AuthContext)
+
+
   return (
     <> <div style={{height: "80vh" }} >
       <img className="homePageImg" src="./public/pasta.webp" alt="" />
+      <h2>Hey {user?.username}</h2>
 <p style={{ alignContent: 'center'}} >Look for fun and colorful recipes, based off 
   what is <br />left in your fridge, your favorite cuisine 
   or preferred diet!</p>
   <p>Become a member and build 
 your own hangry pantry!</p>
 
-    <button><a href="browse">Let's go!</a></button>
+<a style={{backgroundColor:"black", padding:"5px", borderRadius:"3px"}} href="browse">Let's go!</a>
  </div>
     </>
   )
