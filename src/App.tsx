f7d7df7c5ec7d9b043c6cbf5347ad42e9a582271
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-09-20 23:55:37 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import {
   createRoutesFromElements,
   createBrowserRouter,
@@ -6,66 +5,80 @@ import {
   RouterProvider,
   Outlet,
 } from "react-router-dom";
-import '../src/style/App.css'
-import Home from "../views/Home"
-import BrowseRecipes from "../views/BrowseRecipes"
-import MyRecipes from "../views/MyRecipes"
+import "../src/style/App.css";
+import Home from "../views/Home";
+import BrowseRecipes from "../views/BrowseRecipes";
+import MyRecipes from "../views/MyRecipes";
 import RecipeDetails from "../views/RecipeDetails";
-import Login from "../views/Login"
-import Signup from "../views/Signup"
-import Dashboard from "../views/Dashboard"
-import ErrorPage from "../views/ErrorPage"
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
+import Login from "../views/Login";
+import Signup from "../views/Signup";
+import Dashboard from "../views/Dashboard";
+import ErrorPage from "../views/ErrorPage";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 import { RecipeContextProvider } from "./context/RecipeContext";
-import { AuthContextProvider } from './context/AuthContext';
-
-
+import { AuthContextProvider } from "./context/AuthContext";
+import ProtectedRoute from "../components/ProtectedRoute";
+import { app } from "./config/firebaseConfig";
 
 function App() {
-
   const router = createBrowserRouter(
-createRoutesFromElements(
-  <Route path="/" element={<Root/>} errorElement={<ErrorPage/>}>
-    <Route index element={<Home/>}/>
-    <Route path="browse" element={<BrowseRecipes/>}/>
-    <Route path="browse/:recipeId" element={<RecipeDetails/> }/> 
+    createRoutesFromElements(
+      <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
+        <Route index element={<Home />} />
+        <Route path="browse" element={<BrowseRecipes />} />
+        {/* <Route path="browse/:recipeId" element={<RecipeDetails/> }/>  */}
+        {/* //! DO I WANT TO HIDE RECIPES AS WELL OR JUST KEEP IT TO MY RECIPES */}
 
+        <Route
+          path="browse/:recipeId"
+          element={
+            <ProtectedRoute>
+              <RecipeDetails />
+            </ProtectedRoute>
+          }
+        />
 
-    <Route path="my-recipes" element={<MyRecipes/>}/>
-    <Route path="signup" element={<Signup/>}/>
-    <Route path="login" element={<Login/>}/>
-    <Route path="dashboard" element={<Dashboard/>}/>
+        <Route
+          path="my-recipes"
+          element={
+            <ProtectedRoute>
+              <MyRecipes />
+            </ProtectedRoute>
+          }
+        />
 
-
-  </Route>
-)
-
-  )
-
+        <Route path="signup" element={<Signup />} />
+        <Route path="login" element={<Login />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Route>
+    )
+  );
 
   return (
     <>
-
-<AuthContextProvider>
-    <RecipeContextProvider>
-    <RouterProvider router={router}/>
-     <Footer/>  
-     </RecipeContextProvider>
-     </AuthContextProvider>
+      <AuthContextProvider>
+        <RecipeContextProvider>
+          <RouterProvider router={router} />
+          <Footer />
+        </RecipeContextProvider>
+      </AuthContextProvider>
     </>
-  )
+  );
 }
-const Root = () =>{
+const Root = () => {
   return (
     <>
-    <NavBar/> 
-    <img className="mainLogoTxt" src="./public/logo-txt2.png" alt="omNomNom" />
+      <NavBar />
+      <img
+        className="mainLogoTxt"
+        src="./public/logo-txt2.png"
+        alt="omNomNom"
+      />
 
-    <Outlet/>
-
+      <Outlet />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
