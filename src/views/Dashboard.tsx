d4b599336572d4c411a3f0ec077a611c62f9f332
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-09-21 23:46:28 +0200

    Update Dashboard.tsx

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,37 +1,56 @@
 // import React from 'react'
-import '../src/style/dashboard.css'
-import { useNavigate } from 'react-router-dom'
+import { useContext } from "react";
+import "../src/style/dashboard.css";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../src/context/AuthContext";
 
 function Dashboard() {
+  const { user, logOut } = useContext(AuthContext);
+  console.log(user);
 
-  const dashNavigateTo = useNavigate()
+  const dashNavigateTo = useNavigate();
 
-const goBrowseDash = ()=>{
-  dashNavigateTo("/browse")
-}
+  const goBrowseDash = () => {
+    dashNavigateTo("/browse");
+  };
 
-const goMyRecipesDash = ()=>{
-  dashNavigateTo("/my-recipes")
-}
+  const goMyRecipesDash = () => {
+    dashNavigateTo("/my-recipes");
+  };
 
-const goLogOutDash = ()=>{
-  dashNavigateTo("/")
-}
+  const goLogOutDash = () => {
+    logOut();
+    dashNavigateTo("/");
+  };
 
   return (
-    <div className="dashboard" style={{height: "60vh" }}>
-      <h2>Welcome {"user.name"}</h2>
-      <button onClick={goBrowseDash} className="dashboardBtn">Browse</button>
-      <button onClick={goMyRecipesDash} className="dashboardBtn"> My Recipes</button>
-      <button onClick={goLogOutDash} className="dashboardBtn">Log out</button>
+    <div className="dashboard" style={{ height: "60vh" }}>
+      <button
+        style={{
+          fontSize: "18pt",
+          color: "black",
+          backgroundColor: "transparent",
+        }}
+      >
+        ←{" "}
+      </button>
+      <h2 style={{ color: "black" }}>Welcome {user?.email}!</h2>
+      <button onClick={goBrowseDash} className="dashboardBtn">
+        Browse
+      </button>
+      <button onClick={goMyRecipesDash} className="dashboardBtn">
+        {" "}
+        My Recipes
+      </button>
+      <button onClick={goLogOutDash} className="dashboardBtn">
+        Log out
+      </button>
 
-<a style={{marginTop: "5rem" }} href="/">Delete your account</a>
-    
-
-
-
+      <a style={{ marginTop: "5rem" }} href="/">
+        Delete your account
+      </a>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
