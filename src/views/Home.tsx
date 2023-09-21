bc44c1bbfe611f3eafbcacaccdd5b011550f3853
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-09-21 23:47:01 +0200

    Update Home.tsx

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,28 +1,37 @@
 // import React from 'react'
 // import BrowseRecipes from './BrowseRecipes'
 
-import { useContext } from "react"
-import { AuthContext } from "../src/context/AuthContext"
+import { useContext } from "react";
+import { AuthContext } from "../src/context/AuthContext";
 
 function Home() {
-
-  const {user}= useContext(AuthContext)
-
+  const { user } = useContext(AuthContext);
 
   return (
-    <> <div style={{height: "80vh" }} >
-      <img className="homePageImg" src="./public/pasta.webp" alt="" />
-      <h2>Hey {user?.username}</h2>
-<p style={{ alignContent: 'center'}} >Look for fun and colorful recipes, based off 
-  what is <br />left in your fridge, your favorite cuisine 
-  or preferred diet!</p>
-  <p>Become a member and build 
-your own hangry pantry!</p>
+    <>
+      {" "}
+      <div style={{ height: "80vh" }}>
+        <img className="homePageImg" src="./public/pasta.webp" alt="" />
+        <h2>Hey {user?.email}</h2>
+        <p style={{ alignContent: "center" }}>
+          Look for fun and colorful recipes, based off what is <br />
+          left in your fridge, your favorite cuisine or preferred diet!
+        </p>
+        <p>Become a member and build your own hangry pantry!</p>
 
-<a style={{backgroundColor:"black", padding:"5px", borderRadius:"3px"}} href="browse">Let's go!</a>
- </div>
+        <a
+          style={{
+            backgroundColor: "black",
+            padding: "5px",
+            borderRadius: "3px",
+          }}
+          href="browse"
+        >
+          Let's go!
+        </a>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
