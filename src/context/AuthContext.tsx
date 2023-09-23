b5024276c90fcc82c75736ac1d4d4e4cac77284b
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-09-23 17:12:39 +0200

    Update AuthContext.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,21 +1,29 @@
 import { ReactNode, createContext, useEffect, useState } from "react";
 import {
+  GoogleAuthProvider,
   createUserWithEmailAndPassword,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  signInWithPopup,
   signOut,
 } from "firebase/auth";
 import { User } from "../types/customTypes";
-import { auth } from "../config/firebaseConfig";
+import { auth, provider } from "../config/firebaseConfig";
 
 interface AuthContextType {
   user: User | null;
+  loggedUser: User | null;
   setUser: (user: User) => void;
   logOut: () => void;
   signUp: (email: string, password: string) => void;
   logIn: (email: string, password: string) => void;
+  googleLogIn: (email: string, password: string) => void;
 }
 
+//! ADD DISPLAY NAME TO AUTHCONTEXT + SIGNUP PAGE && FIND A
+//! WAY TO ADDING IT TO GOOGLE (find a way to update the user information)
+
+//! FIND A WAY OF KEEPING THE USER LOGGED IN
 
 interface AuthContextProviderProps {
   children: ReactNode;
@@ -23,10 +31,12 @@ interface AuthContextProviderProps {
 
 export const AuthInitContext = {
   user: null,
+  loggedUser: null,
   setUser: () => console.log("not initialized"),
   logOut: () => console.log("not initialized"),
   signUp: () => console.log("not initialized"),
   logIn: () => console.log("not initialized"),
+  googleLogIn: () => console.log("not initialized"),
 };
 
 export const AuthContext = createContext<AuthContextType>(AuthInitContext);
@@ -85,6 +95,30 @@ export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
     });
   };
 
+
+  const googleLogIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        // setUser(result.user);
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+
   useEffect(() => {
     isUserActive();
   }, []);
@@ -100,7 +134,9 @@ export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
   };
 
   return (
-    <AuthContext.Provider value={{ user, setUser, signUp, logIn, logOut }}>
+    <AuthContext.Provider
+      value={{ user, setUser, signUp, logIn, logOut, googleLogIn }}
+    >
       {children}
     </AuthContext.Provider>
   );
