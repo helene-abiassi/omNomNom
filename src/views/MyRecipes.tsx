6f6e24fe27d42ef235526860a567b216cf84293a
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-09-23 17:14:26 +0200

    Update MyRecipes.tsx

--- src/views/MyRecipes.tsx
+++ src/views/MyRecipes.tsx
@@ -1,13 +1,12 @@
-// import React from 'react' 
+// import React from 'react'
 
 function MyRecipes() {
   return (
-    
-    <div style={{height: "80vh"}}>
+    <div style={{ height: "80vh" }}>
       <p>Browse through your favorite recipes!</p>
-      
-      </div>
-  )
+      <p>Nothing saved yet...</p>
+    </div>
+  );
 }
 
-export default MyRecipes
+export default MyRecipes;
