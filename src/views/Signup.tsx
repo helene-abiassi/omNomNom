26c03f197462b63ac05cfa02600a89c24b331e8a
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-09-25 23:54:52 +0200

    Update Signup.tsx

--- src/views/Signup.tsx
+++ src/views/Signup.tsx
@@ -1,20 +1,68 @@
-// import React from 'react'
-import '../src/style/SignupPage.css'
-import SignUp from "../components/SignUp"
-// import LogIn from "../components/LogIn"
+import { useLocation } from "react-router-dom";
+import Comments from "../src/components/Comments";
+import BackButton from "../src/components/BackButton";
+import BackToTop from "../src/components/BackToTop";
 
+function RecipeDetails() {
+  const location = useLocation();
+  const { recipe } = location.state;
+  // console.log("recipe :>> ", recipe);
 
+  const saveToFavorites = () => {
+    console.log("Saved!");
+  };
 
-function Signup() {
+  const handleFavoriteCLick = () => {
+    saveToFavorites();
+  };
+
   return (
-    <div>
-    <SignUp />
+    <div style={{ color: "black" }}>
+      <BackButton />
+      <h3 style={{ textAlign: "center" }}>{recipe.title}</h3>
 
+      <img
+        style={{ width: "40%", marginBottom: "0%" }}
+        src={recipe.image}
+        alt={recipe.name}
+      />
+      <div className="recipeCard">
+        <div>
+          <button onClick={handleFavoriteCLick} className="favIcon">
+            ❤ 0
+          </button>
 
+          <h4>Ingredients:</h4>
+
+          <ul>
+            {recipe.extendedIngredients.map((ingredient, ingInd) => {
+              return (
+                <>
+                  <li key={ingInd}>
+                    {ingredient.original} ({ingredient.measures.metric.amount}{" "}
+                    {ingredient.measures.metric.unitShort})
+                  </li>
+                </>
+              );
+            })}
+          </ul>
+          <h4>Instructions: </h4>
+          <ul>
+            {recipe.analyzedInstructions[0].steps.map((step) => {
+              return (
+                <li style={{ width: "80%" }}>
+                  <strong>Step {step.number}:</strong> {step.step}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <BackToTop />
+      <h3>Comments:</h3>
+      <Comments />
     </div>
-  )
+  );
 }
 
-export default Signup
-
-
+export default RecipeDetails;
