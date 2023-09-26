ffba877c0397f162b1b0f865607f4b0379c57daf
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-09-26 18:43:45 +0200

    Update Comments.tsx

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -2,8 +2,11 @@ import {
   Timestamp,
   addDoc,
   collection,
+  deleteDoc,
+  doc,
   getDocs,
   onSnapshot,
+  orderBy,
   query,
 } from "firebase/firestore";
 // import React from "react";
@@ -11,20 +14,24 @@ import { db } from "../config/firebaseConfig";
 import { ChangeEvent, useContext, useEffect, useState } from "react";
 import "../style/Comments.css";
 import AuthContext from "../context/AuthContext";
+import { useParams } from "react-router-dom";
 
 interface CommentsType {
   author: string;
   message: string;
   date: Timestamp | Date;
+  id?: string;
 }
-
+// potential TS solution for comments with ID and without : one interface(or maybe Type alias) that extends from another one
 function Comments() {
   const { user } = useContext(AuthContext);
   const [comments, setComments] = useState<CommentsType[] | null>(null);
   const [newComment, setNewComment] = useState("");
 
+  const { recipeId } = useParams();
+
   const getComments = async () => {
-    const querySnapshot = await getDocs(collection(db, "comments"));
+    const querySnapshot = await getDocs(collection(db, `${recipeId}`));
 
     const commentsArray: CommentsType[] = [];
     querySnapshot.forEach((doc) => {
@@ -60,24 +67,42 @@ function Comments() {
       date: new Date(),
     };
     // console.log("newCommentPost :>> ", newCommentPost);
-    const docRef = await addDoc(collection(db, "comments"), newCommentPost);
+    // const docRef = await addDoc(collection(db, "comments"), newCommentPost);
+    const docRef = await addDoc(collection(db, `${recipeId}`), newCommentPost);
     console.log("Document written with ID: ", docRef.id);
   };
 
   const getRealTimeComments = () => {
-    const q = query(collection(db, "comments"));
+    const q = query(collection(db, "comments"), orderBy("date", "desc"));
+    // const q = query(collection(db, "71406"), orderBy("date", "desc"));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const commentsArray: CommentsType[] = [];
       querySnapshot.forEach((doc) => {
-        commentsArray.push(doc.data() as CommentsType);
+        console.log("doc :>> ", doc.id);
+        const message = { ...doc.data(), id: doc.id } as CommentsType; // be careful with the use of type assertion (ok, this time was me...)
+        console.log("my new message :>> ", message);
+        commentsArray.push(message);
       });
-      //   console.log("Current cities in CA: ", commentsArray);
       setComments(commentsArray);
+      console.log("commentsArray :>> ", commentsArray);
     });
   };
 
+  const deleteComment = async (commentId) => {
+    const commentsArray: CommentsType[] = [];
+    console.log("commentId :>> ", commentId);
+    try {
+      // const commentId = e.target.id;
+      console.log("delete msgs", commentId);
+      const deletedMeesage = await deleteDoc(doc(db, "comments", commentId));
+      console.log("successfull :>> ", deletedMeesage);
+    } catch (error) {
+      console.log("error", error);
+    }
+    // setComments(commentsArray);
+  };
+
   useEffect(() => {
-    // getComments();
     getRealTimeComments();
   }, []);
 
@@ -95,6 +120,12 @@ function Comments() {
               <p>{comment.author}</p>
               <p>{comment.message}</p>
               <p>{formatDate(comment.date)}</p>
+              {/* <button onClick={deleteComments}>Delete</button> */}
+              {user?.email === comment.author && (
+                <button onClick={() => deleteComment(comment.id)}>
+                  Delete
+                </button>
+              )}
             </div>
           );
         })}
