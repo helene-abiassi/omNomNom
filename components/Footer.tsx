9d2cb36f19f40921bc24eae6131d515ed344439a
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-04 10:39:07 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-
 function Footer() {
   return (
-    <div className='footerBox'>
-      <footer>Copyright Helene Abi Assi, 2023</footer>
-
+    <div className="footerBox">
+      <img className="mainLogo footerImg" src="./public/logo.png" alt="" />
+      <span className="footerText">
+        © Helene Abi Assi, 2023{" "}
+        <a href="https://spoonacular.com/">Spoonacular API</a>
+      </span>
+      <img className="mainLogo footerImg" src="./public/logo.png" alt="" />
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
