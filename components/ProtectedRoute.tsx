4cc6db499272866e8230b9dc27820fab4482769a
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-04 10:45:32 +0200

    Update ProtectedRoute.tsx

--- components/ProtectedRoute.tsx
+++ components/ProtectedRoute.tsx
@@ -1,37 +1,27 @@
-import { ReactNode, useContext, useEffect, useState } from "react";
-import { Navigate } from "react-router-dom";
+import { ReactNode, useContext } from "react";
+// import { Navigate } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import { useIsAuth } from "../hooks/useIsAuth";
 import ProtecterRouteErrorPage from "./ProtecterRouteErrorPage";
+import Loader from "./Loader";
 
 type ProtectedRouteProps = {
   children: ReactNode;
 };
 
 function ProtectedRoute({ children }: ProtectedRouteProps) {
-  console.log("%c protected route", "color:orange");
-
   const { loader } = useContext(AuthContext);
 
   const allowAccess = useIsAuth();
 
-  // const isRedirect = () => {
-  //   setTimeout(() => {
-  //     return 3;
-  //   }, 5000);
-  // };
-  const showme = () => {
-    console.log("loader", loader);
-    console.log("allowAccess", allowAccess);
-  };
   return (
     <div>
       {loader ? (
-        <h1>LOADING....</h1>
+        <Loader />
       ) : allowAccess ? (
         children
       ) : (
-        <>{loader && allowAccess && <ProtecterRouteErrorPage />}</>
+        <ProtecterRouteErrorPage />
       )}
     </div>
   );
