09a33a51913dc7b27bdcebbc6f40eb3b155f5764
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-04 11:01:02 +0200

    Update firebaseConfig.ts

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 
@@ -17,5 +18,6 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 
