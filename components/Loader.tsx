bcfd85898d41a6e297f3d3ff22464d8438df5ebc
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-04 10:44:16 +0200

    Add files via upload

---
+++ components/Loader.tsx
@@ -0,0 +1,16 @@
+// import React from "react";
+
+function Loader() {
+  return (
+    <div>
+      <br />
+      <h3 style={{ color: "#FF5F1F" }}>LOADING....</h3>
+
+      <img className="loader" src="./public/loader.png" alt="" />
+      <br />
+      <br />
+    </div>
+  );
+}
+
+export default Loader;
