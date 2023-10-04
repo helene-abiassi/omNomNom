a0ad4b28e4b3fc25cb2b5d0e8f830681c59b3765
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-04 11:08:44 +0200

    Update Home.tsx

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,45 +1,55 @@
 import { useContext } from "react";
 import { AuthContext } from "../src/context/AuthContext";
 import { Link } from "react-router-dom";
+import Comments from "../src/components/Comments";
 
 function Home() {
   const { user } = useContext(AuthContext);
 
   return (
     <>
-      {" "}
-      <div style={{ height: "80vh" }}>
+      <div style={{ height: "100%" }}>
+        <h3 style={{ color: "black" }}>Hey {user?.displayName}🍕!</h3>
         <img className="homePageImg" src="./public/pasta.webp" alt="" />
-        <h2>Hey {user?.email}</h2>
-
         {/* display dofferent msg for logged in/out */}
+        <br />
+        <br />
         <p style={{ alignContent: "center" }}>
           Look for fun and colorful recipes, based off what is <br />
           left in your fridge, your favorite cuisine or preferred diet!
         </p>
-
+        <p>
+          <a
+            style={{
+              backgroundColor: "#FE347E",
+              padding: "8px",
+              borderRadius: "3px",
+              // color: "black",
+            }}
+            href="/browse"
+          >
+            Let's go!
+          </a>
+        </p>
         {user ? (
           <></>
         ) : (
-          <p>
-            <strong>
-              <Link to={"signup"}>Become a member</Link> and build your own
-              hangry pantry!
-            </strong>
+          <p
+            style={{
+              backgroundColor: "#242424",
+              color: "white",
+              padding: "10px",
+              marginTop: "2rem",
+            }}
+          >
+            <Link to={"/signup"}>
+              Become a member and build your own hangry pantry!
+            </Link>
           </p>
         )}
-
-        <a
-          style={{
-            backgroundColor: "black",
-            padding: "5px",
-            borderRadius: "3px",
-          }}
-          href="browse"
-        >
-          Let's go!
-        </a>
       </div>
+      <br />
+      {/* <Comments /> */}
     </>
   );
 }
