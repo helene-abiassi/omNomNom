2a85a101f3c9f84c94c49d69d7844fb806ee9651
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-04 11:09:39 +0200

    Update RecipeDetails.tsx

--- src/views/RecipeDetails.tsx
+++ src/views/RecipeDetails.tsx
@@ -1,53 +1,96 @@
-// import React from 'react'
-// import { useParams } from 'react-router-dom'
-// import RecipeCards from '../components/RecipeCards';
 import { useLocation } from "react-router-dom";
+import Comments from "../src/components/Comments";
+import BackButton from "../src/components/BackButton";
+import BackToTop from "../src/components/BackToTop";
+import FavoriteButton from "../src/components/FavoriteButton";
+import { useContext, useEffect } from "react";
+import AuthContext from "../src/context/AuthContext";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../src/config/firebaseConfig";
 
 function RecipeDetails() {
   const location = useLocation();
   const { recipe } = location.state;
-  console.log("recipe :>> ", recipe);
+
+  // const { id, image, title, readyInMinutes, servings } = recipe;
+  const { user } = useContext(AuthContext);
+
+  const handleFavoriteClick = async () => {
+    console.log("CLICKED :>> ");
+    const newFavedRecipe: FavoriteRecipeType = {
+      id: recipe.id,
+      image: recipe.image,
+      readyInMinutes: recipe.readyInMinutes,
+      servings: recipe.servings,
+      title: recipe.title,
+      url: `/browse/${recipe.id}`,
+    };
+
+    try {
+      if (user) {
+        const userDocRef = doc(db, "favoriteRecipesCollection", `${user.uid}`);
+        const recipeDocRef = doc(userDocRef, "recipes", `${recipe.id}`);
+        await setDoc(recipeDocRef, newFavedRecipe);
+
+        console.log("Recipe added to favorites!");
+      } else {
+        console.error("User not authenticated.");
+        // You might want to handle the case where the user is not authenticated.
+      }
+    } catch (error) {
+      console.error("Error adding recipe to favorites:", error);
+    }
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <div style={{ color: "black" }}>
-      <h3 style={{ textAlign: "center" }}>{recipe.title}</h3>
+      <div className="backHeader">
+        <BackButton />
+        <h2 style={{ textAlign: "center" }}>{recipe.title}</h2>
+      </div>
 
-      <img
-        style={{ width: "40%", marginBottom: "0%" }}
-        src={recipe.image}
-        alt={recipe.name}
-      />
+      <img className="recipePageImg" src={recipe.image} alt={recipe.name} />
       <div className="recipeCard">
         <div>
-          <button className="favIcon">❤ 0</button>
-
-          <h4>Ingredients:</h4>
+          <div className="recipePageHeader">
+            <h3>Ingredients:</h3>
+            <FavoriteButton onClick={handleFavoriteClick} recipe={recipe} />
+          </div>
 
           <ul>
             {recipe.extendedIngredients.map((ingredient, ingInd) => {
               return (
-                <>
+                <div key={ingInd}>
                   <li key={ingInd}>
                     {ingredient.original} ({ingredient.measures.metric.amount}{" "}
                     {ingredient.measures.metric.unitShort})
                   </li>
-                </>
+                </div>
               );
             })}
           </ul>
-          <h4>Instructions: </h4>
+          <h3>Instructions: </h3>
           <ul>
-            {recipe.analyzedInstructions[0].steps.map((step) => {
+            {recipe.analyzedInstructions[0].steps.map((step, stepInd) => {
               return (
-                <li style={{ width: "80%" }}>
-                  <strong>Step {step.number}:</strong> {step.step}
-                </li>
+                <>
+                  <li key={stepInd} style={{ width: "80%" }}>
+                    <strong>Step {step.number}:</strong> <br />
+                    {step.step}
+                  </li>
+                  {/* <br /> */}
+                </>
               );
             })}
           </ul>
         </div>
       </div>
-      <h3>Comments:</h3>
+      <BackToTop />
+      <Comments />
     </div>
   );
 }
