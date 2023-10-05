3946fa1e85f133faf994744ce0ca8861544c7e7a
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-05 23:53:17 +0200

    Update ProtectedRoute.tsx

--- components/ProtectedRoute.tsx
+++ components/ProtectedRoute.tsx
@@ -1,5 +1,4 @@
 import { ReactNode, useContext } from "react";
-// import { Navigate } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import { useIsAuth } from "../hooks/useIsAuth";
 import ProtecterRouteErrorPage from "./ProtecterRouteErrorPage";
