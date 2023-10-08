4669606de29465e178438bedbf1d9e1f051751cb
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-08 17:25:36 +0200

    add .env

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey:"AIzaSyDy9ysE0IXSoMk7WE2qT44Nr3TKBu2vA2U",
-  authDomain: "nnomnom-hln.firebaseapp.com",
-  projectId: "onnomnom-hln",
-  storageBucket: "onnomnom-hln.appspot.com",
-  messagingSenderId: "211611816598",
-  appId: "1:211611816598:web:acaf1d519f147640f128b7",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 
